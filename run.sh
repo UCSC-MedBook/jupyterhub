e929aaa0a72e8317db4f330d9b83ffdef2b0c600
Michael <rissem@gmail.com>
2016-10-03 10:31:28 -0700

    refactor run script

--- run.sh
+++ run.sh
@@ -1,2 +1,18 @@
+#! /bin/bash
+set -x -e
+
 source ~/.env
-docker run -it --rm --name jupyterhub -p 0.0.0.0:443:443 -v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/config:/config -v /etc/letsencrypt/archive/medbook.zoomdaisy.com:/cert -e "GITHUB_OAUTH_ID=$GITHUB_OAUTH_ID" -e "GITHUB_OAUTH_SECRET=$GITHUB_OAUTH_SECRET" -e "GITHUB_OAUTH_CALLBACK=$GITHUB_OAUTH_CALLBACK"   medbook/jupyterhub jupyterhub --debug -f /config/jupyterhub_config.py
+printenv
+getopts ":d" opt || true
+
+export VOLUMES="-v /var/run/docker.sock:/var/run/docker.sock -v `pwd`/config:/config -v /etc/letsencrypt/archive/medbook.zoomdaisy.com:/cert"
+export ENV_VARS="-e GITHUB_OAUTH_ID=$GITHUB_OAUTH_ID -e GITHUB_OAUTH_SECRET=$GITHUB_OAUTH_SECRET -e GITHUB_OAUTH_CALLBACK=$GITHUB_OAUTH_CALLBACK"
+
+if [ "$opt" -eq "d"]
+then
+  export command="something else"
+else
+  export command="jupyterhub --debug -f /config/jupyterhub_config.py"
+fi
+
+docker run -it --rm --name jupyterhub -p 0.0.0.0:443:443 $VOLUMES $ENV_VARS medbook/jupyterhub $command
